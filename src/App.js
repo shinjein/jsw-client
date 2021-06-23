41b6053db188392d645e454c94876b9baafc5a36
shinjein <shinjein@hu-berlin.de>
2021-06-23 12:06:04 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import ListProjects from "./components/ListProjects";
+import { Switch, Route } from "react-router-dom";
+import ProjectDetails from "./components/ProjectDetails";
+import Navbar from "./components/Navbar";
 
-function App() {
+const App = () => {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Switch>
+        <Route exact path={["/", "/projects"]} component={ListProjects} />
+        <Route exact path="/projects/add" component={AddProject} />
+        <Route exact path="/projects/:id" component={ProjectDetails} />
+      </Switch>
     </div>
-  );
+  );  
 }
+
+
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { logout } from "../api";
+
+function Navbar() {
+
+  return (
+    <>
+      <p className="my-name"> !!!</p>
+      <ul>
+        <li>
+          <NavLink to="/">
+            <button onClick={logoutUser}>Logout</button>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeStyle={{ color: "red" }} exact to="/projects">
+            Projects
+          </NavLink>
+        </li>
+
+      </ul>
+    </>
+  ) 
+}
+
+export default Navbar;
